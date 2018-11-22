7c2b1703727a75faf129eaf0b846cbb9091b0058
Sattvik Chakravarthy <sattvik@gmail.com>
2018-11-22 11:38:29 +0530

    Updating six version.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'six'
+        'six==1.11.0'
     ],
     zip_safe=True,
     entry_points={'console_scripts': [
