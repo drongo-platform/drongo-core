5421f12cccd7842233e16ea1a3729595f0acffd6
Sattvik Chakravarthy <sattvik@gmail.com>
2018-12-11 10:42:35 +0530

    Updating dependency.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='drongo-core',
-    version='1.0.1',
+    version='1.0.2',
     description='A nano web-framework for python.',
     author='Sattvik Chakravarthy',
     author_email='sattvik@gmail.com',
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'six==1.11.0'
+        'six>=1.10.0'
     ],
     zip_safe=True,
     entry_points={'console_scripts': [
